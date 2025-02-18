f6adaccb8504b6dd2af4bccad41b54ebfc3c5a51
nishilcygbit <nishil@cygbit.com>
2025-02-18 15:00:36 +0530

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -69618,68 +69618,63 @@ ${pendingInterceptorsFormatter.format(pending)}
     }
 
     async function parseFiles(client, templatesDir, prefix) {
-      // Read each file in the directory
-      // Make this work such that after every 20th file there is a delay of 5 seconds and then resumes reading the files
       const allFiles = fs.readdirSync(templatesDir);
       core.notice(`Found ${allFiles.length} files in the directory`);
-      // Next split the files into chunks of 20
+
       const filesChunks = [];
       for (let i = 0; i < allFiles.length; i += 20) {
         filesChunks.push(allFiles.slice(i, i + 20));
       }
       core.notice(`Split the files into ${filesChunks.length} chunks`);
+
       for (const fileChunk of filesChunks) {
         core.notice(`Processing the next batch of files - ${fileChunk.length}`);
-        await fileChunk.forEach(async (name, index) => {
+
+        for (const name of fileChunk) {
+          // Use for...of for async/await inside loop
           const path = `${templatesDir}/${name}`;
           core.notice(`Processing file - ${path}`);
 
-          // If it's a directory, read the file within there
           if (fs.lstatSync(path).isDirectory()) {
             readFiles(path);
-            return;
+            continue; // Use continue to skip to the next file
           }
 
-          // Parse the JSON from the file
           const file = JSON.parse(fs.readFileSync(path));
-
           const templateName = `${prefix}${file.Template.TemplateName}`;
 
-          // First, figure out if we have a template
-          await client
-            .send(new GetTemplateCommand({ TemplateName: templateName }))
-            .then(async () => {
-              // We have a template! Update it
-
-              await client
-                .send(
-                  new UpdateTemplateCommand({
-                    Template: { ...file.Template, TemplateName: templateName },
-                  })
-                )
-                .then(() => {
-                  core.notice(`Updated template: ${templateName} (${name})`);
+          try {
+            await client.send(
+              new GetTemplateCommand({ TemplateName: templateName })
+            );
+            // Template exists, update it
+            try {
+              await client.send(
+                new UpdateTemplateCommand({
+                  Template: { ...file.Template, TemplateName: templateName },
                 })
-                .catch((error) => {
-                  core.setFailed(error.message);
-                });
-            })
-            .catch(async () => {
-              await client
-                .send(
-                  new CreateTemplateCommand({
-                    Template: { ...file.Template, TemplateName: templateName },
-                  })
-                )
-                .then(() => {
-                  core.notice(`Created template: ${templateName} (${name})`);
+              );
+              core.notice(`Updated template: ${templateName} (${name})`);
+            } catch (error) {
+              core.setFailed(error.message);
+            }
+          } catch (error) {
+            // Catch the GetTemplateCommand error
+            // Template doesn't exist, create it
+            try {
+              await client.send(
+                new CreateTemplateCommand({
+                  Template: { ...file.Template, TemplateName: templateName },
                 })
-                .catch((error) => {
-                  core.setFailed(error.message);
-                });
-            });
+              );
+              core.notice(`Created template: ${templateName} (${name})`);
+            } catch (createError) {
+              core.setFailed(createError.message);
+            }
+          }
           core.notice(`Finished Processing file - ${path}`);
-        });
+        }
+
         core.notice(
           "Waiting for 5 seconds before processing the next batch of files"
         );
